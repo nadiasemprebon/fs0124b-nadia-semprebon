894730970cbb5bb7ca6d5433844bd99ab90c8d79
nadiasemprebon <semprebonnadia6@gail.com>
2024-03-04 14:49:12 +0000

    s3-g1

---
+++ m2/s3/g1/javaScript.js
@@ -0,0 +1,11 @@
+class User {
+  constructor(_firstName, _lastName, _age, _location) {
+    this.firstName = _firstName;
+    this.lastName = _lastName;
+    this.age = _age;
+    this.location = _location;
+  }
+}
+
+let nadia = new User("Nadia", "Semprebon", 31, "Verona");
+let elena = new User("Elena", "Rossi", 32, "Bologna");
