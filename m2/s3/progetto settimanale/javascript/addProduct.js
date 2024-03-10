00159ece5c625303181a35dbe0dda12c9c4ea4d1
nadiasemprebon <semprebonnadia6@gail.com>
2024-03-10 19:08:38 +0000

    k

--- m2/s3/progetto settimanale/javascript/addProduct.js
+++ m2/s3/progetto settimanale/javascript/addProduct.js
@@ -19,7 +19,7 @@ salva.addEventListener("click", function (e) {
 
   console.log(prodotto);
 
-  fetch("https://striveschool-api.herokuapp.com/api/product/")
+  /*fetch("https://striveschool-api.herokuapp.com/api/product/")
     .then((resp) => {
       if (!resp.ok) {
         throw { httpError: response.status };
@@ -27,19 +27,17 @@ salva.addEventListener("click", function (e) {
       return resp, json();
     })
     .then((data) => console.log(data))
-    .catch((err) => console.log(err));
+    .catch((err) => console.log(err));*/
 });
 
-/*fetch("https://striveschool-api.herokuapp.com/api/product/", {
-    method: "POST",
-    body: JSON.stringify(prodotto),
-    headers: {
-      "Content-Type": "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWVkZWE5MzdiMGFlMjAwMTljYTQ4YmQiLCJpYXQiOjE3MTAwOTA4OTksImV4cCI6MTcxMTMwMDQ5OX0.8FMufJ39GmhA3226S23A3N6S3kYgwUtE3_R7pnCYlh0",
-    },
-  })
-    .then((res) => res.json())
-    .then((res) => console.log(res));
-});
-*/
+fetch("https://striveschool-api.herokuapp.com/api/product/", {
+  method: "POST",
+  body: JSON.stringify(prodotto),
+  headers: {
+    "Content-Type": "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWVkZWE5MzdiMGFlMjAwMTljYTQ4YmQiLCJpYXQiOjE3MTAwOTA4OTksImV4cCI6MTcxMTMwMDQ5OX0.8FMufJ39GmhA3226S23A3N6S3kYgwUtE3_R7pnCYlh0",
+  },
+})
+  .then((res) => res.json())
+  .then((res) => console.log(res));
--- m2/s3/progetto settimanale/javascript/index.js
+++ m2/s3/progetto settimanale/javascript/index.js
@@ -38,3 +38,4 @@ function generaClone() {
 
   return clone;
 }
+generaClone();
