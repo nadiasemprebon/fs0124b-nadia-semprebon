d430fb85e88deb6db2fc3edceb10dc279ed55620
nadiasemprebon <semprebonnadia6@gail.com>
2024-03-10 12:39:23 +0000

    knnk

--- m2/s3/progetto settimanale/javascript/index.js
+++ m2/s3/progetto settimanale/javascript/index.js
@@ -20,7 +20,7 @@ fetch("https://striveschool-api.herokuapp.com/api/product/", {
       let editBtn = riga.querySelector(".edit");
       let infoBtn = riga.querySelector(".info");
 
-      imgUrl.innerText = prodotto.imageUrl; /// serve qualcosa d'altro
+      imgUrl.innerText = prodotto.imgUrl; /// serve qualcosa d'altro
       name.innerText = prodotto.name;
       brand.innerText = prodotto.brand;
       description.innerText = prodotto.description;
