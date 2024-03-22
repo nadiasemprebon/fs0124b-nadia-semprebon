f40fe32fa3ab2b49d37d256b615a0a79960bb0a6
nadiasemprebon <semprebonnadia6@gail.com>
2024-03-22 13:13:21 +0000

    progetto m3-s1

---
+++ m3/s1/progetto settimanale/autosalone/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FiatComponent } from './components/fiat/fiat.component';
+import { FordComponent } from './components/ford/ford.component';
+import { AudiComponent } from './components/audi/audi.component';
+import { HomeComponent } from './components/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    FiatComponent,
+    FordComponent,
+    AudiComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
