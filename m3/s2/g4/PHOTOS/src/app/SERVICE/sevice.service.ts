c38ef7e97a8a8bfb0d7af437eab072638e034fea
nadiasemprebon <semprebonnadia6@gail.com>
2024-03-28 16:19:04 +0000

    jbj

--- m3/s2/g4/PHOTOS/src/app/SERVICE/sevice.service.ts
+++ m3/s2/g4/PHOTOS/src/app/SERVICE/sevice.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { iInterface } from '../MODELS/interface';
+import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SeviceService {
+  apiUrl: string = 'https://jsonplaceholder.typicode.com/photos';
 
-  constructor() { }
+  arrayPictures: iInterface[] = [];
+
+  photoSubject = new Subject<iInterface[]>();
+  $photo = this.photoSubject.asObservable()
+  constructor(private http: HttpClient) {
+    this.getAllPictures().subscribe()
+    getAllPictures(){
+      return this.http.get<iInterface[]>(this.apiUrl)
+    }
+  }
 }
