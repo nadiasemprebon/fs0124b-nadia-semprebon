5a5515da76da8a9e4c494748f25cb4774a4b9763
nadiasemprebon <semprebonnadia6@gail.com>
2024-03-29 13:00:10 +0000

    ff

--- m3/s2/g5/TODOLIST/src/app/app.module.ts
+++ m3/s2/g5/TODOLIST/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './COMPONENTS/user/user.component';
+import { HomeComponent } from './COMPONENTS/home/home.component';
+import { HeaderComponent } from './COMPONENTS/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
