1d32781e8e52f26671bddbf1cb68e3754a929407
nadiasemprebon <semprebonnadia6@gail.com>
2024-03-29 11:35:13 +0000

    progetto

---
+++ m3/s2/progetto settimanale/PROGETTO/src/app/components/todolist/todolist.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todolist',
+  templateUrl: './todolist.component.html',
+  styleUrl: './todolist.component.scss'
+})
+export class TodolistComponent {
+
+}
