f4fecc4afc4788d23fd07a8debca316e58694346
nadiasemprebon <semprebonnadia6@gail.com>
2024-04-01 15:11:51 +0200

    progetto

--- m3/s2/g5/TODOLIST/src/app/COMPONENTS/completed/completed.component.ts
+++ m3/s2/g5/TODOLIST/src/app/COMPONENTS/completed/completed.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Users } from '../../MODELS/users';
+import { Todo } from '../../MODELS/todo';
+import { TodoService } from '../../SERVICES/todo.service';
 
 @Component({
   selector: 'app-completed',
   templateUrl: './completed.component.html',
-  styleUrl: './completed.component.scss'
+  styleUrl: './completed.component.scss',
 })
 export class CompletedComponent {
+  todoArr: Todo[] = [];
+  users: Users[] = [];
 
+  constructor() {}
+
+  ngOnInit() {}
 }
