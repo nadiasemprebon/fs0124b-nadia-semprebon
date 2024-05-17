6027e8df9936052b0b81263a55dd3ff5a8716eff
nadiasemprebon <semprebonnadia6@gail.com>
2024-05-17 17:04:55 +0200

    commit 5

--- back-end/m2/s1/progetto_settimanale/src/main/java/it/epicode/postazioni/entity/AppConfig.java
+++ back-end/m2/s1/progetto_settimanale/src/main/java/it/epicode/postazioni/entity/AppConfig.java
@@ -8,54 +8,42 @@ import java.util.List;
 
 @Configuration
 public class AppConfig {
+
+    // Creazione di un bean per l'utente1 con i dati di Nadia Semprebon
     @Bean(name = "utente1")
     public Utente utente1Bean() {
-        return new Utente("nani1992", "Nadia","Semprebon", "nani1992@gmail.com");
+        return new Utente("nani1992", "Nadia", "Semprebon", "nani1992@gmail.com");
     }
 
+    // Creazione di un bean per l'utente2 con i dati di Selene Leonetti
     @Bean(name = "utente2")
     public Utente utente2Bean() {
-        return new Utente("sally", "Selene","Leonetti","Seleneleonetti@gmail.com");
+        return new Utente("sally", "Selene", "Leonetti", "Seleneleonetti@gmail.com");
     }
 
+    // Creazione di un bean per l'utente3 con i dati di Alessandra Oliosi
     @Bean(name = "utente3")
     public Utente utente3Bean() {
-        return new Utente("alusaura","Alessandra", "Oliosi","alessandra@gmail.com");
+        return new Utente("alusaura", "Alessandra", "Oliosi", "alessandra@gmail.com");
     }
 
+    // Creazione di un bean per l'edificio1 con il nome, indirizzo e città di Palasport
     @Bean(name = "edificio1")
     public Edificio edificio1Bean() {
-        return new Edificio("Palasport","via Giovanni Piatti 13", "VR");
+        return new Edificio("Palasport", "via Giovanni Piatti 13", "VR");
     }
 
+    // Creazione di un bean per l'edificio2 con il nome, indirizzo e città di Stadio Bentegodi
     @Bean(name = "edificio2")
     public Edificio edificio2Bean() {
-        return new Edificio("Stadio Bentegodi","Via Bentegodi 26", "VR");
+        return new Edificio("Stadio Bentegodi", "Via Bentegodi 26", "VR");
     }
 
+    // Creazione di un bean per l'edificio3 con il nome, indirizzo e città di Sala Comunale
     @Bean(name = "edificio3")
     public Edificio edificio3Bean() {
-        return new Edificio("Sala Comunale","piazza Risorgimento 1", "MI");
+        return new Edificio("Sala Comunale", "piazza Risorgimento 1", "MI");
     }
-
-    @Bean(name = "edificio4")
-    public Edificio edificio4Bean() {
-        return new Edificio("Teatro Rondinelle","via Egidio Bersagli", "NA");
-    }
-
-    @Bean(name = "postazione1")
-    public Postazione postazione1Bean() {
-        return new Postazione("La Libertà", TipoPostazione.PRIVATO, 150, edificio1Bean(), true);
-    }
-
-    @Bean(name = "postazione2")
-    public Postazione postazione2Bean() {
-        return new Postazione("I diritti Umani", TipoPostazione.OPENSPACE, 70, edificio2Bean(),false);
-    }
-
-    @Bean(name = "postazione3")
-    public Postazione postazione3Bean() {
-        return new Postazione("La Primavera del Libro", TipoPostazione.SALA_RIUNIONI, 200, edificio3Bean(),true);
-    }
-
 }
+
+
--- back-end/m2/s1/progetto_settimanale/src/main/java/it/epicode/postazioni/entity/PostazioniRunner.java
+++ back-end/m2/s1/progetto_settimanale/src/main/java/it/epicode/postazioni/entity/PostazioniRunner.java
@@ -9,8 +9,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
+//serve per scrivere le db, senza non funziona
+
 @Component
 public class PostazioniRunner implements CommandLineRunner {
+
+    //iniezione delle dipendenze
+
     @Autowired
     private  EdificioService edificioService;
     @Autowired
@@ -21,22 +26,28 @@ public class PostazioniRunner implements CommandLineRunner {
     private ApplicationContext ctx;
     @Autowired
     private UtenteService utenteService;
+
+    //metodo run eseguito dall'app
+
     @Override
     public void run(String... args) throws Exception {
+        // Recupero dei bean Utente dal contesto dell'applicazione
         Utente utente1 = (Utente) ctx.getBean("utente1");
         Utente utente2 = (Utente) ctx.getBean("utente2");
         Utente utente3 = (Utente) ctx.getBean("utente3");
 
-
+// Recupero dei bean Edificio dal contesto dell'applicazione
         Edificio edificio1 = (Edificio) ctx.getBean("edificio1");
         Edificio edificio2 = (Edificio) ctx.getBean("edificio2");
         Edificio edificio3 = (Edificio) ctx.getBean("edificio3");
 
+// Recupero dei bean Postazione dal contesto dell'applicazione
 
         Postazione postazione1 = (Postazione) ctx.getBean("postazione1");
         Postazione postazione2 = (Postazione) ctx.getBean("postazione2");
         Postazione postazione3 = (Postazione) ctx.getBean("postazione3");
 
+// Salvataggio degli utenti nel database tramite UtenteService, poi di edificioService ed infine di postazioneService
         utenteService.save(utente1);
         utenteService.save(utente2);
         utenteService.save(utente3);
